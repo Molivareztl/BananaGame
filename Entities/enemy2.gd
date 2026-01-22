3920ea5712d5eea9c648344cf687a5407edc32a8
Molivareztl <marcos.olivares.t1vl@gmail.com>
2026-01-21 23:32:41 -0300

    NOW IT CAN SHOOT RIGHT

--- Entities/enemy2.gd
+++ Entities/enemy2.gd
@@ -6,29 +6,28 @@ var health = 5
 @onready var pivot = $Pivot
 @onready var h_ray = $Pivot/HurtRay
 @onready var sfx = $Sfx
+@onready var h_timer = $HurtTimer
 @onready var proyectile = preload("res://Entities/proyectile1.tscn")
 var gravity = 20
 var can_hurt = true
 
 func _physics_process(delta):
 	face.look_at(player.global_position)
-	pivot.rotation.y = lerp_angle(pivot.rotation.y, face.rotation.y, 0.05)
-	pivot.rotation.x = lerp_angle(pivot.rotation.x, clamp(face.rotation.x, -180, 360), 0.05) 
+	pivot.rotation.y = face.rotation.y
+	pivot.rotation.x = clamp((face.rotation.x + 0.2), -180, 360)
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	attack()
 	move_and_slide()
-
+	attack()
 func attack():
 	if h_ray.is_colliding() and h_ray.get_collider().is_in_group("player") and can_hurt == true:
 		can_hurt = false
 		var instance = proyectile.instantiate()
 		get_parent().add_child(instance)
 		instance.global_transform = h_ray.global_transform
-		if can_hurt == false:
-			await get_tree().create_timer(2).timeout 
-			can_hurt = true
+		await get_tree().create_timer(1).timeout 
+		can_hurt = true
 
 func hurt(damage):
 	sfx.play()
